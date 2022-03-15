bb99824fde2fc7919dd05336026845e9a6b00e7c
sd.han <sd.han@whyit.co.kr>
2022-03-15 21:25:39 +0900

    220315 modify api&method name

--- app_ajax.py
+++ app_ajax.py
@@ -19,7 +19,7 @@ connection = pymysql.connect(
 
 cursor = connection.cursor()
 
-def select_user():
+def select_multi_user():
     cursor.execute((
         "select u.login_id, u.user_name, c.company_name "
         "from user_sdhan as u "
@@ -30,22 +30,13 @@ def select_user():
     result = cursor.fetchall()
     return { "result" : result }
 
-def select_login_id(login_id):
+def select_single_user(login_id):
     cursor.execute((
         "select u.login_id, u.user_name, c.company_name "
         "from user_sdhan as u "
         "left join company_sdhan as c "
         "on u.company_id = c.company_id "
         "where login_id like '" + login_id + "' "
-        "limit 1;"
-    ))
-    result = cursor.fetchall()
-    return { "result" : result }
-
-def select_company_name():
-    cursor.execute((
-        "select company_id, company_name "
-        "from company_sdhan "
     ))
     result = cursor.fetchall()
     return { "result" : result }
@@ -54,7 +45,6 @@ def delete_user(login_id):
     cursor.execute((
         "delete from user_sdhan "
         "where login_id like '" + login_id + "' "
-        "limit 1;"
     ))
     connection.commit()
     return "0"
@@ -72,37 +62,32 @@ def update_user(login_id, user_name, company_name):
         "update user_sdhan "
         "set user_name='" + user_name + "', company_id='" + company_name + "' "
         "where login_id like '" + login_id + "' "
-        "limit 1;"
     ))
     connection.commit()
     return "0"
 
-@app.route('/select', methods=['GET'])
-def select_user_api():
-    return select_user()
+@app.route('/user/api/list', methods=['GET'])
+def select_user_multi_api():
+    return select_multi_user()
 
-@app.route('/select/login_id', methods=['GET'])
-def select_login_id_api():
+@app.route('/user/api/get', methods=['GET'])
+def select_user_single_api():
     login_id = request.args.get('login_id')
-    return select_login_id(login_id)
+    return select_single_user(login_id)
 
-@app.route('/delete', methods=['POST'])
+@app.route('/user/api/delete', methods=['POST'])
 def delete_user_api():
     login_id = request.form['login_id']
     return delete_user(login_id)
 
-@app.route('/insert', methods=['POST'])
+@app.route('/user/api/add', methods=['POST'])
 def insert_user_api():
     login_id = request.form['login_id']
     user_name = request.form['user_name']
     company_name = request.form['company_name']
     return insert_user(login_id, user_name, company_name)
 
-@app.route('/insert/company_name', methods=['GET'])
-def insert_company_name_api():
-    return select_company_name()
-
-@app.route('/update', methods=['POST'])
+@app.route('/user/api/update', methods=['POST'])
 def update_user_api():
     login_id = request.form['login_id']
     user_name = request.form['user_name']
@@ -110,29 +95,29 @@ def update_user_api():
     return update_user(login_id, user_name, company_name)
 
 
-@app.route('/user/list_view', methods=['GET', 'POST'])
+@app.route('/user/list_view', methods=['GET'])
 def list_view():
     now = datetime.now()
-    return render_template('list_view.html', now=now)
+    return render_template('user_list_view.html', now=now)
 
-@app.route('/user/detail_view/<login_id>', methods=['GET', 'POST'])
+@app.route('/user/detail_view/<login_id>', methods=['GET'])
 def detail_view(login_id):
     now = datetime.now()
-    return render_template('detail_view.html', login_id=login_id, now=now)
+    return render_template('user_detail_view.html', login_id=login_id, now=now)
 
-@app.route('/user/insert_view', methods=['GET', 'POST'])
+@app.route('/user/insert_view', methods=['GET'])
 def insert_view():
     now = datetime.now()
-    return render_template('insert_view.html', now=now)        
+    return render_template('user_insert_view.html', now=now)        
 
-@app.route('/user/update_view/<login_id>', methods=['GET', 'POST'])
+@app.route('/user/update_view/<login_id>', methods=['GET'])
 def update_view(login_id):
     now = datetime.now()
-    return render_template('update_view.html', login_id=login_id, now=now)
+    return render_template('user_update_view.html', login_id=login_id, now=now)
 
 
 # company_sdhan
-def select_company():
+def select_multi_company():
     cursor.execute((
         "select company_id, company_name "
         "from company_sdhan "
@@ -141,12 +126,11 @@ def select_company():
     result = cursor.fetchall()
     return { "result" : result }
 
-def select_company_id(company_id):
+def select_single_company(company_id):
     cursor.execute((
         "select company_id, company_name "
         "from company_sdhan "
         "where company_id like '" + company_id + "' "
-        "limit 1;"
     ))
     result = cursor.fetchall()
     return { "result" : result }
@@ -155,7 +139,6 @@ def delete_company(company_id):
     cursor.execute((
         "delete from company_sdhan "
         "where company_id like '" + company_id + "' "
-        "limit 1;"
     ))
     connection.commit()
     return "0"
@@ -173,55 +156,54 @@ def update_company(company_id, company_name):
         "update company_sdhan "
         "set company_name='" + company_name + "' "
         "where company_id like '" + company_id + "' "
-        "limit 1;"
     ))
     connection.commit()
     return "0"
 
 
-@app.route('/company/select', methods=['GET'])
-def select_company_api():
-    return select_company()
+@app.route('/company/api/list', methods=['GET'])
+def select_multi_company_api():
+    return select_multi_company()
 
-@app.route('/company/select/company_id', methods=['GET'])
-def select_company_id_api():
+@app.route('/company/api/get', methods=['GET'])
+def select_single_company_api():
     company_id = request.args.get('company_id')
-    return select_company_id(company_id)
+    return select_single_company(company_id)
 
-@app.route('/company/delete', methods=['POST'])
+@app.route('/company/api/delete', methods=['POST'])
 def delete_company_api():
     company_id = request.form['company_id']
     return delete_company(company_id)
 
-@app.route('/company/insert', methods=['POST'])
+@app.route('/company/api/insert', methods=['POST'])
 def insert_company_api():
     company_id = request.form['company_id']
     company_name = request.form['company_name']
     return insert_company(company_id, company_name)
 
-@app.route('/company/update', methods=['POST'])
+@app.route('/company/api/update', methods=['POST'])
 def update_company_api():
     company_id = request.form['company_id']
     company_name = request.form['company_name']
     return update_company(company_id, company_name)
 
 
-@app.route('/company/list_view', methods=['GET', 'POST'])
+@app.route('/company/list_view', methods=['GET'])
 def company_list_view():
     now = datetime.now()
     return render_template('company_list_view.html', now=now)
 
-@app.route('/company/detail_view/<company_id>', methods=['GET', 'POST'])
+@app.route('/company/detail_view/<company_id>', methods=['GET'])
 def company_detail_view(company_id):
     now = datetime.now()
     return render_template('company_detail_view.html', company_id=company_id, now=now)
 
-@app.route('/company/insert_view', methods=['GET', 'POST'])
+@app.route('/company/insert_view', methods=['GET'])
 def company_insert_view():
     now = datetime.now()
     return render_template('company_insert_view.html', now=now)        
 
-@app.route('/company/update_view/<company_id>', methods=['GET', 'POST'])
+@app.route('/company/update_view/<company_id>', methods=['GET'])
 def company_update_view(company_id):
     now = datetime.now()
     return render_template('company_update_view.html', company_id=company_id, now=now)
